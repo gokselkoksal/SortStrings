b201485c164940434367cf1fe966861923b066a6
Göksel Köksal <goksel@getdrop.com>
2018-06-11 11:17:43 +0100

    Initial Commit

---
+++ LocalizableMerge/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LocalizableMerge
+//
+//  Created by Goksel Koksal on 11/06/2018.
+//  Copyright © 2018 Goksel Koksal. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
